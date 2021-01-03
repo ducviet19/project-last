4590c3a52bb260b9ea62909146d66cd258bd637c
ducviet19 <54992665+ducviet19@users.noreply.github.com>
2021-01-03 17:14:22 +0700

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -13,7 +13,7 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-mongoose.connect(process.env.MONGODB_URL || 'mongodb+srv://ducviet98:ducviet98@cluster0.o9tfx.mongodb.net/ducviet98?retryWrites=true&w=majority', {
+mongoose.connect(process.env.MONGODB_URL || 'mongodb://localhost/amazona', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useCreateIndex: true,
